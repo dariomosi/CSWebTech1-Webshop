d4c6010a1a844789cdf24a88bd4e364a8673741f
Dario <dario.mosimann@wmail.ch>
2017-08-07 19:21:01 +0200

    Created a standard page for the beginning with a new style

--- Webshop/index.php
+++ Webshop/index.php
@@ -1,3 +1,12 @@
-<!DOCTYPE html><html><head><meta charset="utf-8"><title>Login</title></head><body>
-<h1>Business Streamline</h1>
+<!DOCTYPE html><html><head><meta charset="utf-8"><title>Login</title>
+<link href="CSS/style.css" rel="stylesheet" type="text/css"/></head><body>
+    <header>
+        <h1>Business Streamline<h1/>
+    </header>
+<div id="main">
+    <article>Article</article>
+    <nav>Nav</nav>
+    <aside>Aside</aside>
+</div>
+<footer>Footer</footer>
 </body></html>
